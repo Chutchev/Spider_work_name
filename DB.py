d48ef110b272c55e64995f9e90229275fd445e47
Chutchev <iChutchev98@gmail.com>
2019-03-17 22:22:15 +0500

    Добавлено обновление БД

--- DB.py
+++ DB.py
@@ -1,20 +1,41 @@
 import sqlite3
+import datetime
 
-
-def insert_to_db(info):
+def select_into_db(url, table_name):
     with sqlite3.connect("URLS.db") as conn:
-        for count, (url, status) in enumerate(info.items()):
-            if len(status['URLS']) == 0:
-                params = (count, url, 'Пустая/битая/ведет на другой ресурс')
-                conn.execute("INSERT INTO CHECKEDURLS (id, url, status) values (?, ?, ?)", params)
+        cur = conn.cursor()
+        data = cur.execute(f"SELECT * FROM {table_name} WHERE url='{url}'")
+        stroke = data.fetchall()
+    if len(stroke) > 0:
+        return True
+    else:
+        return False
 
 
-def create_db():
+def update_db(info, table_name):
     with sqlite3.connect("URLS.db") as conn:
-        conn.execute("""CREATE TABLE CHECKEDURLS(
-        id integer,
-        url varchar(255),
-        status varchar(20)
+        cur = conn.cursor()
+        for url in info:
+            if url is not None:
+                if select_into_db(url, table_name):
+                    cur.execute(f"""UPDATE {table_name} SET url={url}, status='Пустая/неработающая ссылка', 
+                    last_check={datetime.date.today()}""")
+                else:
+                    insert_to_db(url, 'Пустая/неработающая ссылка', table_name)
+
+
+def insert_to_db(url, status, table_name):
+    with sqlite3.connect("URLS.db") as conn:
+        last_check = datetime.date.today()
+        conn.execute(f"INSERT INTO {table_name} (url, status, last_check) VALUES (?, ?, ?)", (url, status, last_check))
+
+
+def create_db(table_name):
+    with sqlite3.connect("URLS.db") as conn:
+        conn.execute(f"""CREATE TABLE {table_name}(
+        url varchar(1000),
+        status varchar(255),
+        last_check DATE
         );""")
 
 
@@ -22,5 +43,5 @@ def main():
     pass
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
