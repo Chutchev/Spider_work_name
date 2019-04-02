d396691e57cd0f4523a8cba18c0463723ed8c0d1
Chutchev <iChutchev98@gmail.com>
2019-04-02 23:15:37 +0500

    Полностью переписан алгоритм обхода сайта

--- script.py
+++ script.py
@@ -3,10 +3,12 @@ import requests
 import time
 import argparse
 import DB
+import sqlite3
+from pprint import pprint
 import requests.exceptions as re
 from sqlite3 import OperationalError
 VERIFIED_URLS = {}
-SITE = ''
+URLS = []
 
 
 def timer(func):
@@ -17,74 +19,128 @@ def timer(func):
     return wrapper
 
 
-def check_other_site_url(doc):
-    soup = BeautifulSoup(doc, features='html.parser')
-    urls = soup.find_all('a')
-    if len(urls) > 0:
-        return True
-    else:
-        return False
-
-
 def read_html(url):
     try:
         try:
             response = requests.get(url=url)
-            doc = response.content.decode('utf-8', errors='ignore')
-            return str(doc)
+            return response
         except (ConnectionError, re.ConnectionError):
-            doc = None
-            return doc
+            pass
     except (re.MissingSchema, re.InvalidSchema):
-        return "Invalid URL"
+        return None
 
 
-def find_urls(doc, filename, amount):
-    urls = []
-    VERIFIED_URLS[f"{SITE}{filename}"] = {'URLS': urls}
-    if doc is not None:
-        soup = BeautifulSoup(doc, features='html.parser')
-        urls = soup.find_all('a')
-        VERIFIED_URLS[f"{SITE}{filename}"] = urls
-        for url in urls:
-            if url.get('href') not in VERIFIED_URLS.keys() and amount > 0:
-                print(f"Осталось ссылок: {amount}")
-                amount -= 1
-                if str(url.get('href')).startswith("http") and not str(url.get('href')).startswith(SITE):
-                    new_lines = read_html(f"{url.get('href')}")
-                    if check_other_site_url(new_lines):
-                        continue
-                    else:
-                        VERIFIED_URLS[url.get('href')] = []
-                else:
-                    new_lines = read_html(f"{SITE}{url.get('href')}")
-                    find_urls(new_lines, url.get('href'), amount)
-            else:
-                break
+def return_url(doc):
+    soup = BeautifulSoup(doc, features='html.parser')
+    urls = soup.find_all('a')
+    return urls
+
+
+def find_urls(doc):
+    soup = BeautifulSoup(doc, features='html.parser')
+    urls = soup.find_all('a')
+    return urls
+
+
+def a_list(site, url, dicionary):
+    if str(url).startswith('http'):
+        response = read_html(url)
+        if response.status_code == 200:
+            doc = response.content.decode('utf-8', errors='ignore')
+            dicionary[url] = find_urls(doc)
+        else:
+            print(response.status_code, url)
+            dicionary[url] = []
+    elif str(url).startswith('/'):
+        response = read_html(f'{site}{url}')
+        if response.status_code == 200:
+            doc = response.content.decode('utf-8', errors='ignore')
+            dicionary[f'{site}{url}'] = find_urls(doc)
+        else:
+            print(response.status_code, url)
+            dicionary[f'{site}{url}'] = []
     else:
-        VERIFIED_URLS[f"{SITE}{filename}"] = []
+        response = read_html(f'{site}/{url}')
+        if response.status_code == 200:
+            doc = response.content.decode('utf-8', errors='ignore')
+            dicionary[f'{site}/{url}'] = find_urls(doc)
+        else:
+            print(response.status_code, url)
+            dicionary[f'{site}/{url}'] = []
+    return dicionary
+
+
+def fill_base_dict(urls, site):
+    for url in urls:
+        url = url.get('href')
+        if url not in VERIFIED_URLS.keys():
+            print(f"{url}")
+            dictionary = a_list(site, url, VERIFIED_URLS)
+            try:
+                VERIFIED_URLS[url] = dictionary.values()
+            except AttributeError:
+                VERIFIED_URLS[url] = []
+
+
+def fill_extra_dict(site):
+    extra_dict = {}
+    for value in VERIFIED_URLS.values():
+        for url in value:
+            try:
+                url = url.get('href')
+                if url not in VERIFIED_URLS.keys():
+                    print(f'fill_extra_dict: {url}')
+                    dictionary = a_list(site, url, extra_dict)
+                    try:
+                        extra_dict[url] = dictionary.values()
+                    except AttributeError:
+                        pass
+            except AttributeError:
+                pass
+    return extra_dict
+
+
+def select_all_into_db(table_name):
+    with sqlite3.connect("URLS.db") as conn:
+        cur = conn.cursor()
+        data = cur.execute(f"SELECT * FROM {table_name}")
+        strokes = data.fetchall()
+    return strokes
 
 
 @timer
 def main():
     parser = argparse.ArgumentParser(description="Spider-script")
     parser.add_argument('site', help='Сайт который надо обойти')
-    parser.add_argument('-amount', '-a', help='Количество переходов по ссылкам по всему сайту', required=False,
-                        default=1000)
     args = parser.parse_args()
-    SITE = args.site
-    amount = int(args.amount)
-    doc = read_html(SITE)
-    find_urls(doc, SITE, amount)
-    table_name = SITE.split("/")[2].split('.')
+    site_name = args.site
+    table_name = site_name.split("/")[2].split('.')
+    print(table_name)
+    site = "http://" + '.'.join(table_name)
+    print(site)
     table_name = "_".join(table_name)
-    dont_working = [k for k, v in VERIFIED_URLS.items() if len(v) == 0 and k != 'None']
+    response = read_html(site_name)
+    full_dict = {}
+    if response.status_code == '404':
+        VERIFIED_URLS[site_name] = []
+    else:
+        doc = response.content.decode('utf-8', errors='ignore')
+        urls = find_urls(doc)
+        fill_base_dict(urls, site)
+        extra_dict = fill_extra_dict(site)
+        full_dict = {**VERIFIED_URLS, **extra_dict}
+    dont = {}
+    for k, v in full_dict.items():
+        if len(v) == 0:
+            dont[k] = [].append(v)
+    pprint(dont)
     try:
         DB.create_db(table_name)
     except OperationalError:
         pass
-    DB.update_db(dont_working, table_name)
-
+    DB.update_db(dont.values(), table_name)
+    strokes = select_all_into_db(table_name)
+    pprint(strokes)
 
 if __name__ == "__main__":
     main()
