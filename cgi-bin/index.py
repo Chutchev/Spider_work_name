662cbd49d399cd0c1490b99b104066a9bc52ca35
Chutchev <iChutchev98@gmail.com>
2019-04-02 23:17:31 +0500

    Папка со скриптами сайта

---
+++ cgi-bin/index.py
@@ -0,0 +1,36 @@
+import cgi
+import subprocess
+import sqlite3
+
+def select_all_into_db(table_name):
+    with sqlite3.connect("URLS.db") as conn:
+        cur = conn.cursor()
+        data = cur.execute(f"SELECT * FROM {table_name}")
+        strokes = data.fetchall()
+    return strokes
+
+
+def main():
+    form = cgi.FieldStorage()
+    url = form.getfirst("URL")
+    amount = form.getfirst("amount")
+    table_name = url.split("/")[2].split('.')
+    table_name = "_".join(table_name)
+    subprocess.call(f"python script.py {url}")
+    strokes = select_all_into_db(table_name)
+    print("Content-type: text/html\n")
+    print("""<!DOCTYPE HTML>
+                    <html>
+                    <head>
+                        <meta charset="windows-1251">
+                        <title>Результаты</title>
+                    </head>
+                    <body>""")
+    print('<div id="header"><h1>Вывод</h1></div>')
+    for url, status, date in strokes:
+        print(f'<p>URL: <a href="{url}">{url}</a>    Статус: {status}    Дата: {date}</p>')
+    print('/body')
+
+
+if __name__ == '__main__':
+    main()
