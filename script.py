1bcb7570bbb333028092484afd5e7d14ff0ee99b
ivan <ichutchev98@gmail.com>
2019-07-10 09:09:10 +0500

    Логика поиска title's и создание .py файлов с классами

--- script.py
+++ script.py
@@ -1,13 +1,63 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+from datetime import datetime
 import argparse
-import DB
-import sqlite3
-from pprint import pprint
 import requests.exceptions as re
-from sqlite3 import OperationalError
 import logging
+import os
+
+
+checked = []
+site_name = ""
+logging.basicConfig(filename="logs.log", level=logging.INFO)
+
+
+def spider(ss):
+    global checked
+    global site_name
+    response = read_html(ss)
+    doc = response.content.decode('utf-8', errors='ignore')
+    urls = [x.get('href') for x in find_info(doc, 'a') if x.get('href') not in checked]
+    logging.info(f"Ссылка: {ss}. На сайте: {urls}. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
+    while True:
+        for url in urls:
+            if url is not None:
+                if not url.startswith(site_name) and not url.startswith("http"):
+                    if url not in checked:
+                        res = check_url(f"{site_name}{url}")
+                        if res:
+                            checked.append(url)
+                            spider(f"{site_name}{url}")
+                    else:
+                        break
+                else:
+                    check_url(url)
+                    checked.append(url)
+        break
+
+
+def create_py(title: str):
+    pattern = f"""class {title.capitalize()}:
+    
+    def __init__(self):
+        pass"""
+
+    with open(os.path.abspath(f"./Classes/{title.capitalize()}.py"), "w") as f:
+        f.write(pattern)
+
+    logging.info(f"Класс {title.capitalize()} создан. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
+
+
+def get_title(url):
+    response = read_html(url)
+    if response is not None:
+        title = find_info(response.content.decode('utf-8', errors='ignore'), 'title')
+        title = str(*title).replace("</title>", "").replace("<title>", "")
+        try:
+            create_py(title)
+        except FileExistsError:
+            logging.info(f"Класс {title.capitalize()} уже создан. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
 
 
 def timer(func):
@@ -29,108 +79,36 @@ def read_html(url):
         return None
 
 
-def find_urls(doc):
+def find_info(doc, tag):
     soup = BeautifulSoup(doc, features='html.parser')
-    urls = soup.find_all('a')
-    return urls
+    info = soup.find_all(tag)
+    return info
 
 
 def check_url(url):
     try:
         response = requests.get(url)
         if 199 < response.status_code < 400 and response is not None:
+            print(url, response.status_code)
             return True
         else:
             print(url, response.status_code)
             return False
-    except re.ConnectionError as e:
+    except (re.ConnectionError, re.MissingSchema) as e:
         print(url, e)
         return False
 
-
-def fill_base_list(urls, url_list: list):
-    for url in urls:
-        url = url.get('href')
-        if url not in url_list:
-            url_list.append(url)
-    return url_list
-
-
-def fill_breaking_url_list(urls, site):
-    breaking = []
-    for url in urls:
-        if str(url).startswith('http'):
-            if not check_url(url):
-                breaking.append(url)
-        elif str(url).startswith('/'):
-            if not check_url(f'{site}{url}'):
-                breaking.append(f'{site}{url}')
-        else:
-            if not check_url(f'{site}/{url}'):
-                breaking.append(f'{site}/{url}')
-    return breaking
-
-
-def select_all_into_db(table_name):
-    with sqlite3.connect("URLS.db") as conn:
-        cur = conn.cursor()
-        data = cur.execute(f"SELECT * FROM {table_name}")
-        strokes = data.fetchall()
-    return strokes
-
-
-def func(base_list, site):
-    print('func')
-    for url in base_list:
-        print(url)
-        if str(url).startswith(site):
-            doc = read_html(url)
-            logging.error("{url} startswith")
-            if doc is not None:
-                doc = doc.content.decode('utf-8', errors='ignore')
-                urls = find_urls(doc)
-                a_list = fill_base_list(urls, base_list)
-                for extra_url in a_list:
-                    if extra_url not in base_list:
-                        print(extra_url)
-                        base_list.append(extra_url)
-        else:
-            logging.error(f"ERROR: {url} in base_list")
-    return base_list
-
 @timer
 def main():
-    logging.basicConfig(filename="log.log")
+    os.makedirs(os.path.abspath("./Classes"))
     parser = argparse.ArgumentParser(description="Spider-script")
     parser.add_argument('site', help='Сайт который надо обойти')
     args = parser.parse_args()
+    global site_name
     site_name = args.site
-    table_name = site_name.split("/")[2].split('.')
-    site = "http://" + '.'.join(table_name)
-    table_name = "_".join(table_name).replace("-", '__')
-    response = read_html(site_name)
-    doc = response.content.decode('utf-8', errors='ignore')
-    urls = find_urls(doc)
-    new_list = fill_base_list(urls, [])
-    base_list = [site_name, *new_list]
-    old = []
-    while old != base_list:
-        old = base_list
-        print("old: ", old)
-        print("base_list: ", base_list)
-        base_list = func(base_list, site)
-        if base_list == old:
-            break
-    breaking = fill_breaking_url_list(base_list, site)
-    pprint(breaking)
-    try:
-        DB.create_db(table_name)
-    except OperationalError:
-        pass
-    try:
-        DB.update_db(breaking, table_name)
-    except Exception  as e:
-        print(e)
+    spider(site_name)
+    for url in checked:
+        get_title(url)
 
 
 if __name__ == "__main__":
