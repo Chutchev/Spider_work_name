90c6bf9760b0bf438d1a973fdbd3e22632d4fafb
ivan <ichutchev98@gmail.com>
2019-07-17 16:25:09 +0500

    для переноса на другой комп

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import threading
 from queue import Queue
+from queue import Empty
 import time
 from datetime import datetime
 import argparse
@@ -34,44 +35,55 @@ def check_class(title:str):
     else:
         return False
 
-
+# TODO ИСПРАВИТЬ, НЕ РАБОТАЕТ КАК НУЖНО МНЕ QUE.GET()
 def run(que, checked):
+    while True:
+        url = check_url(que)
+        logging.info(f"\t\tQUE: {list(que.queue)}")
+        if not url:
+            logging.info(f"\t\tBREAKBREAKBREAKBREAKBREAK")
+            break
+        else:
+            checked.put(url)
+            spider(url, que, checked)
+    que.task_done()
+
+
+def check_url(que):
+    print(que.queue)
+    try:
+        url = que.get(False)
+        return url
+    except Empty:
+        return False
+
+
+def spider(link, que, checked):
+    global SITE_NAME
     options = Options()
-    logging.basicConfig(filename="logs.log", level=logging.INFO)
     options.add_argument('--headless')
     driver = webdriver.Chrome(os.path.abspath("./chromedriver"), chrome_options=options)
-    spider(que.get(), driver, que, checked)
-    que.join()
-
-
-@thread
-def spider(link, driver, que, checked):
-    global SITE_NAME
-    logging.getLogger()
-    print(link, threading.current_thread().name, threading.active_count())
-    logging.info(f"Проверяем {link}. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
-    if que.empty():
-        que.task_done()
     driver.get(link)
-    checked.put(link)
-    print("CHECKED", checked.queue)
-    create_class(driver.title)
     elements = driver.find_elements_by_xpath("//a")
     for element in elements:
-        try:
-            url = element.get_attribute('href')
-            if url is not None and url not in que.queue and url.startswith(SITE_NAME) and url not in checked.queue:
-                que.put(url)
-        except StaleElementReferenceException as e:
+        check_element(element, que, checked)
+    logging.getLogger()
+    print(link, threading.current_thread().name, threading.active_count(), len(checked.queue))
+    logging.info(f"Проверяем {link}. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
+    logging.info(f"\t\tCHECKED: {list(checked.queue)}")
+    driver.quit()
 
-            continue
-    print(que.queue)
-    que.task_done()
+
+def check_element(element, que, checked):
+    logging.getLogger()
     try:
-        while not que.empty():
-            spider(que.get(), driver, que, checked)
-    except IndexError:
-        print("УРААА")
+        url = element.get_attribute('href')
+        print(url, f"Thread name: {threading.current_thread().name}, active Threads: {threading.active_count()}")
+        if url is not None and url not in que.queue and url.startswith(SITE_NAME) and url not in checked.queue:
+            que.put(url)
+            logging.info(f"Такой url: {url} уже в списках. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
+    except StaleElementReferenceException as e:
+        logging.info(e, f"Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
 
 
 def create_py(pattern, title: str):
@@ -84,14 +96,15 @@ def create_py(pattern, title: str):
 
 def create_class(title:str):
     pattern = """
-    class {}:
+    # {0}
+    class {1}:
 
         xpath_dict = dict()
 
         def __init__(self):
             pass"""
     logging.getLogger()
-    pattern = pattern.format(title)
+    pattern = pattern.format(datetime.now().strftime('%d-%m-%Y %H:%M:%S'), title)
     try:
         if not check_class(title):
             create_py(pattern, title)
@@ -103,10 +116,19 @@ def create_class(title:str):
             f"Ошибка про создании класса. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
 
 
+def create_threads(que, checked):
+    for _ in range(4):
+        t = threading.Thread(target=run, args=(que, checked))
+        t.daemon = True
+        t.start()
+    # que.task_done()
+
+
 @timer
 def main():
     que = Queue()
     checked = Queue()
+    logging.basicConfig(filename="logs.log", level=logging.INFO)
     logging.info(f"\nНОВЫЙ ЗАПУСК. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}\n")
     try:
         os.makedirs(os.path.abspath("./Classes"))
@@ -119,7 +141,7 @@ def main():
     try:
         SITE_NAME = args.site
         que.put(SITE_NAME)
-        run(que, checked)
+        create_threads(que, checked)
         que.join()
     except Exception as e:
         print(e)
