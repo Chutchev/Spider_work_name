b3a9e562df4f301f67d06cdf4020b06c5e112f65
ivan <ichutchev98@gmail.com>
2019-07-18 13:18:10 +0500

    Переведено на многопоточность

--- script.py
+++ script.py
@@ -6,12 +6,11 @@ import argparse
 import requests.exceptions as re
 import logging
 import os
+import threading
+from queue import Queue, Empty
 
 
-checked = []
-site_name = ""
-logging.basicConfig(filename="logs.log", level=logging.INFO)
-pattern = ""
+SITE_NAME = ""
 
 
 def timer(func):
@@ -24,32 +23,65 @@ def timer(func):
 
 def create_right_url(url):
     if url is not None:
-        if not url.startswith(site_name) and not url.startswith("http"):
-            return f"{site_name}{url}"
+        if not url.startswith(SITE_NAME) and not url.startswith("http"):
+            return f"{SITE_NAME}{url}"
         else:
             return url
     else:
         return None
 
 
-def spider(ss):
-    global checked
-    global site_name
-    checked = list(set(checked))
-    response = read_html(ss)
-    print(ss, ss in checked)
-    doc = response.content.decode('utf-8', errors='ignore')
-    create_class(doc, pattern)
-    urls = [create_right_url(x.get('href')) for x in find_info(doc, 'a') if create_right_url(x.get('href')) is not None
-            and create_right_url(x.get('href')) not in checked]
-    logging.info(f"Ссылка: {ss}. На сайте: {urls}. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
-    for url in urls:
-            if url.startswith(site_name):
-                if url not in checked:
-                    checked.append(url)
-                    spider(url)
-                else:
-                    break
+def run(que, checked, errors):
+    while True:
+        url = check_url(que)
+        logging.info(f"\t\tQUE: {list(que.queue)}, THREAD={threading.current_thread().name}")
+        if not url:
+            logging.info(f"\t\tBREAKBREAKBREAKBREAKBREAK")
+            break
+        else:
+            if url not in errors:
+                checked.put(url)
+                spider(url, que, checked, errors)
+    que.task_done()
+
+
+def check_url(que):
+    try:
+        res = que.get(timeout=20)
+    except Empty:
+        res = False
+    print(res, threading.current_thread().name)
+    return res
+
+
+def spider(url, que, checked, errors):
+    global SITE_NAME
+    response = read_html(url, errors)
+    if response is not None:
+        if response.status_code == 200:
+            info = find_info(response.content.decode(encoding='utf-8', errors='ignore'), 'a')
+            for link in info:
+                if link is not None:
+                    link = link.get('href')
+                    link = create_right_url(link)
+                    print("link -> ", link)
+                    add_element_to_queue(link, que, checked, errors)
+            logging.getLogger()
+            print(url, threading.current_thread().name, threading.active_count(), len(checked.queue))
+            logging.info(f"Проверяем {url}. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
+            logging.info(f"\t\tCHECKED: {list(checked.queue)}, \tTHREAD: {threading.current_thread().name}")
+        else:
+            errors.add(url)
+    que.task_done()
+
+
+def add_element_to_queue(url, que, checked, errors):
+    logging.getLogger()
+    print(url, f"Thread name: {threading.current_thread().name}, active Threads: {threading.active_count()}")
+    if url is not None and url not in que.queue and url.startswith(SITE_NAME) and url not in checked.queue and \
+            url not in errors:
+        que.put(url)
+        logging.info(f"Добавлен url: {url}. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
 
 
 def create_py(pattern, title: str):
@@ -64,11 +96,16 @@ def get_title(doc):
     return title
 
 
-def read_html(url):
+def read_html(url, errors):
     try:
         try:
             response = requests.get(url=url)
-            return response
+            if response.status_code == 200:
+                print("\t\tREAD_HTML URL ->", url)
+                return response
+            else:
+                errors.add(url)
+                return None
         except (ConnectionError, re.ConnectionError):
             pass
     except (re.MissingSchema, re.InvalidSchema):
@@ -93,9 +130,19 @@ def create_class(doc, pattern):
             f"Ошибка про создании класса. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
 
 
+def create_threads(func, *args):
+    for _ in range(4):
+        t = threading.Thread(target=func, args=args)
+        t.daemon = True
+        t.start()
+
 @timer
 def main():
-    logging.info(f"НОВЫЙ ЗАПУСК. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}\n")
+    que = Queue()
+    checked = Queue()
+    errors = set()
+    logging.basicConfig(filename="logs.log", level=logging.INFO)
+    logging.info(f"\nНОВЫЙ ЗАПУСК. Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}\n")
     try:
         os.makedirs(os.path.abspath("./Classes"))
     except FileExistsError:
@@ -103,13 +150,17 @@ def main():
     parser = argparse.ArgumentParser(description="Spider-script")
     parser.add_argument('site', help='Сайт который надо обойти')
     args = parser.parse_args()
-    global site_name
-    global checked
-    global pattern
-    with open(os.path.abspath("./Шаблон.txt"), 'r') as f:
-        pattern = f.read()
-    site_name = args.site
-    spider(site_name)
+    global SITE_NAME
+    try:
+        SITE_NAME = args.site
+        que.put(SITE_NAME)
+        create_threads(run, que, checked, errors)
+        que.join()
+        print(len(checked.queue), len(errors))
+        checked = set(checked.queue) - errors
+        logging.info(f"\t\tCHEKED -> {checked} (Количество ссылок: {len(checked)}). Время: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
